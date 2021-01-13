1680efd55ed6c10dfdb8ed032ff1c9bf6495a0d6
contakrg <77363215+contakrg@users.noreply.github.com>
2021-01-13 11:52:55 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Your gateway to terraform.
+  Welcome to ${PREFIX}'s app. Your gateway to terraform cloud or terraform enterprise.
   <!-- END -->
 
   </div>
